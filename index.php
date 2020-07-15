9a247c42c8c61e5de34cf404d75f5ddde51ae8ae
sudo-christopher <christopher.cpereira@gmail.com>
2020-07-14 22:22:55 -0300

    url-get com tratamento da pagina 404

--- index.php
+++ index.php
@@ -51,92 +51,22 @@
         </div><!--center fim-->
     </header><!--cabecalho fim-->
 
-    <section class="banner">
+    <?php
+    //lembrando que colocamos valores a serem passados nos a href enfrente a o include_path, por isso consegue pegar valores da url la,podiamos atribuir a home la mas nao ha necessidade devido ao tratamento que vamos fazer
+    //agr o tratamento que o htacess fazia ainda funciona, mas dessa vez estamos tratando ele de uma maneira diferente.
+    //se a url existir... ? pegue a url... : se nao existir atribua home
+    //get pega as coisas via url por isso usamos ela aqui para esse tipo de tratamento
+        $url = isset($_GET['url']) ? $_GET['url'] : 'home';
 
-            <h2 class="mensagem center">Bem Vindo!</h2>
-
-    </section><!--banner imagem fim-->
-
-    <section class="autor">
-        <div class="center">
-            <div class="w50 left">
-                <h2>Christopher Pereira</h2>
-                <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo.</p>
-                <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo.</p>
-            </div><!--w50-->
-
-            <div class="w50 left">
-                <img class="bola-foto right" src="<?php echo INCLUDE_PATH;?>img/eu.jpeg"><!--minha imagem fim-->
-            </div><!--w50-->
-            <div class="clear"></div>
-        </div><!--center fim-->
-    </section><!--descricao e foto do autor fim-->
-
-    <section class="especialidades"> 
-        <div class="center">
-            <h3 class="titulo-especialidades">Especidalidades</h3>
-            <div class="w50 left conteudo-especialidade">
-                <h3><i class="fab fa-html5"></i></h3>
-                <h3>HTML</h3>
-                <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido.</p>
-            </div><!--conteudo da especialidade fim-->
-
-            <div class="w50 left conteudo-especialidade">
-                <h3><i class="fab fa-css3-alt"></i></h3>
-                <h3>CSS</h3>
-                <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido.</p>
-            </div><!--conteudo da especialidade fim-->
-
-            <div class="w50 left conteudo-especialidade">
-                <h3><i class="fab fa-js"></i></h3>
-                <h3>JAVASCRIPT</h3>
-                <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido.</p>
-            </div><!--conteudo da especialidade fim-->
-
-            <div class="w50 left conteudo-especialidade">
-                <h3><i class="fab fa-php"></i></h3>
-                <h3>PHP</h3>
-                <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido.</p>
-            </div><!--conteudo da especialidade fim-->
-        </div><!--center fim-->
-        <div class="clear"></div>
-    </section><!--especialidades fim-->
-
-    <section class="curriculo">
-        <div class="center">
-            <div class="w50 left">
-                <h2 class="titulo">Experiencia Proficional</h2>
-                <div class="single">
-                    <p class="emprego">Lorem ipsum</p>
-                    <p class="emprego-descricao">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI.</p>
-                </div><!--single fim-->
-                <div class="single">
-                    <p class="emprego">Lorem ipsum</p>
-                    <p class="emprego-descricao">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI.</p>
-                </div><!--single fim-->
-                <div class="single">
-                    <p class="emprego">Lorem ipsum</p>
-                    <p class="emprego-descricao">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI.</p>
-                </div><!--single fim-->
-            </div><!--w50-->
-
-            <div class="w50 left">
-                <h2 class="titulo">Projetos</h2>
-                <div class="projeto">
-                    <ul>
-                        <li>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI. Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI. Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI.</li>
-                    </ul>
-                </div><!--projeto fim-->
-                <div class="projeto">
-                    <ul>
-                        <li>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI. Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI. Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI.</li>
-                    </ul>
-                </div><!--projeto fim-->
-            </div><!--w50-->
-            <div class="clear"></div>
-        </div><!--center fim-->
-    </section><!--carreira e projetos fim-->
-
+    //se o arquivo X dentro de page com o nome da url.php existir faca
+        if(file_exists('pages/'.$url.'.php')){
+            include('pages/'.$url.'.php');
+        }else{
+            //podemos incluir uma pagina de erro 404 por exemplo
+            include('pages/404.php');
+        }
+    ?>
+    
     <footer>
         <div class="center">
             <p>Todos os direitos reservados</p>
---
+++ pages/home.php
@@ -0,0 +1,85 @@
+<section class="banner">
+
+            <h2 class="mensagem center">Bem Vindo!</h2>
+
+    </section><!--banner imagem fim-->
+
+    <section class="autor">
+        <div class="center">
+            <div class="w50 left">
+                <h2>Christopher Pereira</h2>
+                <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo.</p>
+                <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo.</p>
+            </div><!--w50-->
+
+            <div class="w50 left">
+                <img class="bola-foto right" src="<?php echo INCLUDE_PATH;?>img/eu.jpeg"><!--minha imagem fim-->
+            </div><!--w50-->
+            <div class="clear"></div>
+        </div><!--center fim-->
+    </section><!--descricao e foto do autor fim-->
+
+    <section class="especialidades"> 
+        <div class="center">
+            <h3 class="titulo-especialidades">Especidalidades</h3>
+            <div class="w50 left conteudo-especialidade">
+                <h3><i class="fab fa-html5"></i></h3>
+                <h3>HTML</h3>
+                <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido.</p>
+            </div><!--conteudo da especialidade fim-->
+
+            <div class="w50 left conteudo-especialidade">
+                <h3><i class="fab fa-css3-alt"></i></h3>
+                <h3>CSS</h3>
+                <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido.</p>
+            </div><!--conteudo da especialidade fim-->
+
+            <div class="w50 left conteudo-especialidade">
+                <h3><i class="fab fa-js"></i></h3>
+                <h3>JAVASCRIPT</h3>
+                <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido.</p>
+            </div><!--conteudo da especialidade fim-->
+
+            <div class="w50 left conteudo-especialidade">
+                <h3><i class="fab fa-php"></i></h3>
+                <h3>PHP</h3>
+                <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido.</p>
+            </div><!--conteudo da especialidade fim-->
+        </div><!--center fim-->
+        <div class="clear"></div>
+    </section><!--especialidades fim-->
+
+    <section class="curriculo">
+        <div class="center">
+            <div class="w50 left">
+                <h2 class="titulo">Experiencia Proficional</h2>
+                <div class="single">
+                    <p class="emprego">Lorem ipsum</p>
+                    <p class="emprego-descricao">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI.</p>
+                </div><!--single fim-->
+                <div class="single">
+                    <p class="emprego">Lorem ipsum</p>
+                    <p class="emprego-descricao">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI.</p>
+                </div><!--single fim-->
+                <div class="single">
+                    <p class="emprego">Lorem ipsum</p>
+                    <p class="emprego-descricao">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI.</p>
+                </div><!--single fim-->
+            </div><!--w50-->
+
+            <div class="w50 left">
+                <h2 class="titulo">Projetos</h2>
+                <div class="projeto">
+                    <ul>
+                        <li>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI. Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI. Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI.</li>
+                    </ul>
+                </div><!--projeto fim-->
+                <div class="projeto">
+                    <ul>
+                        <li>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI. Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI. Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI.</li>
+                    </ul>
+                </div><!--projeto fim-->
+            </div><!--w50-->
+            <div class="clear"></div>
+        </div><!--center fim-->
+    </section><!--carreira e projetos fim-->
