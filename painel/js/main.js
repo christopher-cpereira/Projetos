6965e6014bb3a7f42033b342340ad1d1c99fce2f
sudo-christopher <christopher.cpereira@gmail.com>
2020-08-06 21:07:42 -0300

    terminado efeito do menu para se ocultar do painel

--- painel/js/main.js
+++ painel/js/main.js
@@ -10,14 +10,22 @@ $(function(){
     $('.menubotao').click(function(){
         if(open){
             //se estiver aberto precisamos fechar
-            $('.menu').animate({'width':'0'});
-            $('.content').animate({'left':'0'});
-            $('.content').animate({'width':'100%'});
-            $('header').animate({'left':'0'});
-            $('header').animate({'width':'100%'});
+            $('.menu').animate({'width':0, 'padding':0}, function(){
+                open = false;
+            });
+            $('.content,header').css('width','100%');
+            $('.content,header').animate({'left':0}, function(){
+                open = false;
+            });
         }else{
-            //menu fechado
-
+            //menu fechado vamos abri-lo
+            $('.menu').animate({'width':'300px', 'padding':'10px'}, function(){
+                open = true;
+            });
+            $('.content,header').css('width','calc(100% - 300px)');
+            $('.content,header').animate({'left':'300px'}, function(){
+                open = true;
+            });
         }
     })
 
